50f577bb8bc4a1f0b7ba5337f505c1782ba080ad
Harald Weppner <harald.weppner@tidalscale.com>
2015-06-10 17:59:20 -0700

    update virtualenv name and path

--- mplw.sh
+++ mplw.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-MY_VIRTUAL_ENV=${MY_VIRTUAL_ENV:-pyenv-editing}
+MY_VIRTUAL_ENV=${MY_VIRTUAL_ENV:-editing}
 
 # Wrapper script to avoid asciidoc magic
 # to ruby and python filter commands (ending in .rb and .py),
 # which would not allow sourcing the correct virtualenv Python environment
-source ~/${MY_VIRTUAL_ENV}/bin/activate
+source ~/virtualenvs/${MY_VIRTUAL_ENV}/bin/activate
 CWD=$(dirname $0)
 python ${CWD}/mplw.py $@
